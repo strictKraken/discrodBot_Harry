5f1f2d9139a4c53b9a8078cb924e0dd54df10658
Stephan <StefanStark77@gmail.com>
2021-11-12 16:32:48 +0300

    init project

--- bot_main.py
+++ bot_main.py
@@ -5,7 +5,7 @@ import time
 # import logging
 from discord.ext import commands
 
-#Для рандома
+# Для рандома
 random.seed(time.time())
 bot = commands.Bot(command_prefix='соси ', intents=discord.Intents.all())
 
@@ -15,35 +15,34 @@ async def bibu(message):
     if message.channel.id == config.CHANNEL_ID:
         print('start move people')
         voice_channel_id = config.VOICE_CHANNEL_ID.values()
-        #Лист каналов для взаимодействия с ними
+        # Лист каналов для взаимодействия с ними
         voice_channel = []
         for channel in voice_channel_id:
             voice_channel.append(bot.get_channel(channel))
 
-        #Получение всех активных участников каналов
-        members = []
+        # Получение всех активных участников каналов
+        # members = []
         for channel in voice_channel:
-            #Найти просто участников
+            # Найти просто участников
             tmp_channel_mem = channel.members
-            #Dывести иx id
+            # Вывести иx id
             for membr in tmp_channel_mem:
                 await membr.move_to(voice_channel[random.randrange(len(voice_channel))])
-                #members.append(membr.id)
-        #print(members)
+                # members.append(membr.id)
+        # print(members)
 
-
-
-        #members = voice_channel_id.members
-        #memids = []
-        #for member in members:
+        # members = voice_channel_id.members
+        # memids = []
+        # for member in members:
         #    memids.append(member.id)
-        #print(memids)
+        # print(memids)
     #    await message.channel.send('booms')
 
 
 @bot.event
 async def on_ready():
     print('Im ready to work')
+
 
 bot.run(config.TOKEN)
 # logging.basicConfig(level=logging.INFO)1
